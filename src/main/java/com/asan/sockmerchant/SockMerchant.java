2be0e1c67a388b2b3ad4b3aa39c0e1a0e7ae4428
Nesrinulgay <nsrn.ulgy@gmail.com>
2020-01-06 10:26:51 +0300

    SockMerchant Algorithm Solution

---
+++ src/main/java/com/asan/sockmerchant/SockMerchant.java
@@ -0,0 +1,76 @@
+package com.asan.sockmerchant;
+
+import java.util.*;
+
+/**
+ * Created by Nesrin Aşan on 1/3/2020.
+ */
+public class SockMerchant {
+    static List<Integer> corapList = new ArrayList();
+    static int toplamCorapCiftSayisi;
+    static Iterator<Integer> iter;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kaç adet çorap girilecek");
+        int corapSayisiGir = scanner.nextInt();
+
+        /**
+         * Çorapları topla.
+         */
+        int sayac = 1;
+        while (true) {
+            System.out.println("Çorap numarası giriniz");
+            int corap = scanner.nextInt();
+            corapList.add(corap);
+            if (sayac == corapSayisiGir) {
+                break;
+            }
+            sayac++;
+        }
+        iter = corapList.iterator();
+
+        /**
+         * Çorapları gör
+         */
+        getCoraplar(corapList);
+        /**
+         *  Topladığın çoraplardan çiftleri ayır.
+         */
+        corapCiftleriniBul();
+        System.out.println("Toplam çorap çift sayısı " + toplamCorapCiftSayisi);
+
+    }
+
+    private static void getCoraplar(List<Integer> corapList) {
+        for (Integer corap : corapList) {
+            System.out.println(corap + ", ");
+        }
+    }
+
+    private static void corapCiftleriniBul() {
+
+        /**
+         * Metot recursive olduğu için "java.util.ConcurrentModificationException" hatası alıyordu.
+         * en dıştaki for döngüsü bitse bile tekra içerideki for döngüsüne giriyor. Çünkü recursive.
+         * Bu nedenle etiketli break kullandım.
+         */
+        sonunaKadarCik:
+        for (Integer corap : corapList) {
+            int aynıCorapSayisi = 0;
+            for (Integer kontrolEdilenCorap : corapList) {
+                if (corap == kontrolEdilenCorap) {
+                    aynıCorapSayisi++;
+                    if (aynıCorapSayisi == 2) {
+                        toplamCorapCiftSayisi++;
+                        corapList.remove(corap);
+                        corapCiftleriniBul();
+                        break sonunaKadarCik;
+                    }
+                }
+            }
+
+        }
+    }
+
+}
